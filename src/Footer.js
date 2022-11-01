e000b5a1d3693424ae0d908bd48d697f95fc1063
Tomasz Jąder <tomasz.jader@gmail.com>
2022-11-01 15:42:25 +0100

    fix and add footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
+import './Footer.css';
+import facebook from './facebook.svg'
+import instagram from './instagram.svg'
+import github from './github.svg'
 
 const Footer = () => {
-    return ( 
-        <div>
-            Footer
+    return (
+        <div className='App'>
+            <h2 className='App-header'>
+                Contact me
+            </h2>
+            <p className='App-concret'>
+                I’m open for new opportunities. If you have one for me we should talk!
+            </p>
+            <div className='Social-buttons'>
+                <div className='Social-button'>
+                    <img src={facebook} />
+                </div>
+                <div className='Social-button'>
+                    <img src={instagram} />
+                </div>
+                <div className='Social-button'>
+                    <img src={github} />
+                </div>
+                <div className='Sent-button'>maciej@cotenfrontend.pl</div>
+            </div>
+
+            
         </div>
-     );
+    );
 }
- 
+
 export default Footer;
