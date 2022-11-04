afb32bb49479522d8d0ddf2b137382d6f9b99430
Tomasz Jąder <tomasz.jader@gmail.com>
2022-11-04 18:35:16 +0100

    fix project info

--- src/Projects.js
+++ src/Projects.js
@@ -18,7 +18,7 @@ const Projects = () => {
     return (
         <div id='projects' className='Projects'>
             <h2 className='ProjectHeader'>Recent projects</h2>
-            <p className='ProjectParagraf'>I started my frontend developer path in 2003. Since then, I’ve built some projects. Here’s the list of the things I’ve done so far.</p>
+            <p className='ProjectParagraf'>I started my frontend developer path in 2020. Since then, I’ve built some projects. Here’s the list of the things I’ve done so far.</p>
             <div className='ProjectsGap'>
                 <div className='ProjectsGitHubCards'>
                     <GitHubCard
