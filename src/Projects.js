29e89a3dbb841faff68c069cc69d93d3f8382c1a
Tomasz Jąder <tomasz.jader@gmail.com>
2024-10-30 22:06:10 +0100

    fix gitHubData

--- src/Projects.js
+++ src/Projects.js
@@ -21,31 +21,31 @@ const Projects = () => {
     return (
         <div id='projects' className='Projects'>
             <h2 className='ProjectHeader'>Recent projects</h2>
-            <p className='ProjectParagraf'>I started my frontend developer path in 2020. Since then, I’ve built multiple projects. Here’s the list of some of the things I’ve done so far.</p>
+            <p className='ProjectParagraf'>I started my developer path in 2020. Since then, I’ve built multiple projects. Here’s the list of some of the things I’ve done so far.</p>
             <div className='ProjectsGap'>
                 <div className='ProjectsGitHubCards'>
                     <GitHubCard
                         project={gitHubData.length?gitHubData[0].name:''}
                         description={gitHubData.length?gitHubData[0].description:''}
-                        website={gitHubData.length?gitHubData[0].website : ''}
-                        link={gitHubData.length?gitHubData[0].link :''} />
+                        website={gitHubData.length?gitHubData[0].homepage : ''}
+                        link={gitHubData.length?gitHubData[0].html_url :''} />
                     <GitHubCard
                         project={gitHubData.length?gitHubData[1].name:''}
                         description={gitHubData.length?gitHubData[1].description:''}
-                        website={gitHubData.length?gitHubData[1].website : ''}
-                        link={gitHubData.length?gitHubData[1].link :''} />
+                        website={gitHubData.length?gitHubData[1].homepage : ''}
+                        link={gitHubData.length?gitHubData[1].html_url :''} />
                 </div>
                 <div className='ProjectsGitHubCards'>
                 <GitHubCard
                         project={gitHubData.length?gitHubData[2].name:''}
                         description={gitHubData.length?gitHubData[2].description:''}
-                        website={gitHubData.length?gitHubData[2].website : ''}
-                        link={gitHubData.length?gitHubData[2].link :''} />
+                        website={gitHubData.length?gitHubData[2].homepage : ''}
+                        link={gitHubData.length?gitHubData[2].html_url :''} />
                     <GitHubCard
                         project={gitHubData.length?gitHubData[3].name:''}
                         description={gitHubData.length?gitHubData[3].description:''}
-                        website={gitHubData.length?gitHubData[3].website : ''}
-                        link={gitHubData.length?gitHubData[3].link :''} />
+                        website={gitHubData.length?gitHubData[3].homepage: ''}
+                        link={gitHubData.length?gitHubData[3].html_url :''} />
                 </div>
             </div>
 
